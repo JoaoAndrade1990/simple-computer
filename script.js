1af3a040c75488ba3f9b247963ecba0471b5ce42
JoaoAndrade1990 <joaomagalhaesandrade@gmail.com>
2023-07-12 13:20:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let firstValue = Number(prompt("Enter the first value")) // number
+let operator = prompt("Enter the operator") // string
+let secondValue = Number(prompt("Enter the second value")) // number
+
+// console.log(typeof firstValue)
+// console.log(typeof operator)
+// console.log(typeof secondValue)
+// console.log(parseInt(firstValue)+parseInt(secondValue))
+
+console.log(firstValue+secondValue)
